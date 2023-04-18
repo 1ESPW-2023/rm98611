072c9031a5a0a29c4a90dfd41f980a4a882c28f8
Guilherme-Daher <RM98611@fiap.com.br>
2023-04-18 20:22:21 -0300

    alterações feitas em js e html em aula

--- js/script.js
+++ js/script.js
@@ -1,16 +1,35 @@
 //Exemplo de funções
-const divElement = document.getElementById("div-box")
+// const divElement = document.getElementById("div-box")
 
 // divElement.addEventListener("click", ()=>{
 //     console.log(divElement);
 // });
 
-divElement.addEventListener("click", mostraElemento);
-function mostraElemento(){
-   console.log(divElement)
-}
-function soma (){
-    let n1 = 10
-    let n2 = 5
-    console.log(n1*n2)
-}
+// divElement.addEventListener("click", mostraElemento);
+// function mostraElemento(){
+//    console.log(divElement)
+// }
+// function soma (){
+//     let n1 = 10
+//     let n2 = 5
+//     console.log(n1*n2)
+// }
+//Recuperando elementos do HTML com querrySelector
+// const liElements = document.querySelectorAll("li");
+// //Convertendo NODE LIST em array com spread
+// const arrayElements =[...liElements];
+// console.log(liElements);
+
+const arrayElements = [...document.querySelectorAll("li")];
+arrayElements.map((el,key)=>{
+    console.log(el.textContent.lastIndexOf("1"))
+
+      if(el.textContent.indexOf != (-1)) {
+        el.textContent =`[${el.textContent}]`;
+      }                                                
+});
+console.log(arrayElements);
+
+
+
+
