bbc9e84aecf9c99723804398e2d9c70ad5849e6c
Guilherme-Daher <Rm98611@fiap.com.br>
2023-04-25 20:46:24 -0300

    final

--- js/script.js
+++ js/script.js
@@ -20,16 +20,76 @@
 // const arrayElements =[...liElements];
 // console.log(liElements);
 
-const arrayElements = [...document.querySelectorAll("li")];
-arrayElements.map((el,key)=>{
-    console.log(el.textContent.lastIndexOf("1"))
+// const arrayElements = [...document.querySelectorAll("li")];
+// arrayElements.map((el,key)=>{
+//     console.log(el.textContent.lastIndexOf("1"))
 
-      if(el.textContent.indexOf != (-1)) {
-        el.textContent =`[${el.textContent}]`;
-      }                                                
-});
-console.log(arrayElements);
+//       if(el.textContent.indexOf != (-1)) {
+//         el.textContent =`[${el.textContent}]`;
+//       }                                                
+// });
+// console.log(arrayElements);
 
+let tmp = 0;
+
+function mudaCor() {
+    let r = Math.ceil(Math.random()*255);
+    let g = Math.ceil(Math.random()*255);
+    let b = Math.ceil(Math.random()*255);
+    
+    const elemento = [...document.getElementsByClassName("conteudo")];
+    elemento.forEach( (el)=>{
+        el.style.backgroundColor = `rgb(${r},${g},${b})`;
+    } )
+
+   tmp = setTimeout(mudaCor, 1000);
+
+}
+
+function alteraBanner1() {
+    
+    // let nr = Math.ceil(Math.random() *3);
+    const img1 = document.querySelector(".l-d > img");
+    const img2 = document.querySelector(".l-e > img");
+    img1.src = "./img/banner-lateral-1.png";
+    img2.src = "./img/banner-lateral-2.png";
+
+    setTimeout(alteraBanner2, 1000);
+
+}
+ 
+function alteraBanner2() {
+    
+    // let nr = Math.ceil(Math.random() *3);
+    const img1 = document.querySelector(".l-d > img");
+    const img2 = document.querySelector(".l-e > img");
+    img1.src = "./img/banner-lateral-2.png";
+    img2.src = "./img/banner-lateral-1.png";
+
+    setTimeout(alteraBanner3, 1000);
+
+}
+
+function alteraBanner3() {
+    
+    // let nr = Math.ceil(Math.random() *3);
+    const img1 = document.querySelector(".l-d > img");
+    const img2 = document.querySelector(".l-e > img");
+    img1.src = "./img/banner-lateral-1.png";
+    img2.src = "./img/banner-lateral-2.png";
+
+    setTimeout(alteraBanner1, 1000);
+
+}
+
+alteraBanner1();
+
+
+function turnInOff() {
+    
+    
+
+}
 
 
 
